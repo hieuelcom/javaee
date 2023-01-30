cedba9eb1cdff5c7b32a749a909e928b28e434d3
hieuelcom <112467458+hieuelcom@users.noreply.github.com>
2023-01-30 15:52:07 +0700

    ụk

---
+++ struts/src/java/my_struts/HelloWorldAction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_struts;
+
+/**
+ *
+ * @author young
+ */
+public class HelloWorldAction {
+
+    private String name;
+    private String customGreeting;
+
+    public String execute() throws Exception {
+        setCustomGreeting("Hi! my name is Struts Action Conrol... Hello " + getName());
+        if (name.length() > 0) {
+            return "success";
+        }
+        return "no name";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCustomGreeting() {
+        return customGreeting;
+    }
+
+    public void setCustomGreeting(String customGreeting) {
+        this.customGreeting = customGreeting;
+    }
+
+}
